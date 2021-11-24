1cb8f5e0211edd8d589f7990ed08e9ad1d8f0bca
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-24 15:41:31 -0800

    Add project files.

---
+++ VisualStudioAssignment/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var myVideo;
+
+window.onload = function () {
+    myVideo = document.getElementById("myVideo");
+}
+
+var littleSize = true; //width is 560
+function changeWidth() {
+    let id = null;
+    var increment = 30;
+    clearInterval(id);
+    id = setInterval(changeS, 10);
+    function changeS() {
+        if (increment <= 0) {
+            littleSize = !littleSize;
+            clearInterval(id);
+        } else {
+            if (littleSize) {
+                myVideo.width = parseInt(myVideo.width) + 10;
+                myVideo.height = parseInt(myVideo.height) + 10;
+            } else {
+                myVideo.width = parseInt(myVideo.width) - 10;
+                myVideo.height = parseInt(myVideo.height) - 10;
+            }
+        }
+        increment--;
+    }
+}
